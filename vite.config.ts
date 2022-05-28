67e96a2c2631391df2a119f67a5432569a7dca4e
wgumenyuk <wlad@gumenyuk.de>
2022-05-28 11:03:41 +0200

    Imports organisiert

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { sveltePreprocess } from "svelte-preprocess/dist/autoProcess";
 import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import sveltePreprocess from "svelte-preprocess";
 
 export default defineConfig({
     plugins: [
